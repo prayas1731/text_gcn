2f33f4efa9e26758c255828f2487f557d8b81298
prayas1731 <32987072+prayas1731@users.noreply.github.com>
2021-05-05 13:36:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from layers import *
 from metrics import *
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
-flags = tf.app.flags
+flags = tf.compat.v1.flags
 FLAGS = flags.FLAGS
 
 

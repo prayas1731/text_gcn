aac007b8c6d2e4d23eff2f3843324a0478352299
prayas1731 <32987072+prayas1731@users.noreply.github.com>
2021-05-05 13:38:00 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 def masked_softmax_cross_entropy(preds, labels, mask):
     """Softmax cross-entropy loss with masking."""
